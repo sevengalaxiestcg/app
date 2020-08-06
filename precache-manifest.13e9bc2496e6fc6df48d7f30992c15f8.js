47b28191e74fc41eaa7c6923d71001e63e0e2837
AndreGT500 <andregt500@yahoo.com.br>
2020-08-06 19:51:16 -0300

    Updates

---
+++ precache-manifest.13e9bc2496e6fc6df48d7f30992c15f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1e2ee5b4eb012c8706ff739ddf7706",
+    "url": "/sevengalaxies-app/index.html"
+  },
+  {
+    "revision": "13f13943f0f25faa9dbc",
+    "url": "/sevengalaxies-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51dea97990772c155529",
+    "url": "/sevengalaxies-app/static/js/2.587b26e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sevengalaxies-app/static/js/2.587b26e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f13943f0f25faa9dbc",
+    "url": "/sevengalaxies-app/static/js/main.e633e357.chunk.js"
+  },
+  {
+    "revision": "257d6810d8b70f1519ef",
+    "url": "/sevengalaxies-app/static/js/runtime-main.bb4424a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sevengalaxies-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
