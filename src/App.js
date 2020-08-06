7093cfee8000524530739b633ca7a0c536541ef6
AndreGT500 <andregt500@yahoo.com.br>
2020-08-06 19:36:01 -0300

    o inicio

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Avante Seven Galaxies!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sevengalaxiestcg.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SevenGalaxies.com
         </a>
       </header>
     </div>
