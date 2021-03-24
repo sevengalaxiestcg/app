0c499ce1cb68c153955d747051a79987234a47d6
AndreGT500 <andregt500@yahoo.com.br>
2021-03-23 21:17:06 -0300

    Updates

--- precache-manifest.01ae27849bb7ecdd5fb98d70deb287d7.js
+++ precache-manifest.01ae27849bb7ecdd5fb98d70deb287d7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e48759a341729ac0cb52743691886be2",
+    "revision": "a85738680802e7279d64526e8f39afaa",
     "url": "/app/index.html"
   },
   {
-    "revision": "9018e72fea8a1b530118",
-    "url": "/app/static/css/main.8f80d3ee.chunk.css"
+    "revision": "4c8cf7e1ad82ccc418ab",
+    "url": "/app/static/css/main.48514d85.chunk.css"
   },
   {
     "revision": "9f982cbb06248b2d47ff",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app/static/js/2.4d2f5541.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9018e72fea8a1b530118",
-    "url": "/app/static/js/main.facb1768.chunk.js"
+    "revision": "4c8cf7e1ad82ccc418ab",
+    "url": "/app/static/js/main.61025f2d.chunk.js"
   },
   {
     "revision": "fb683d993c694aeb9312",
